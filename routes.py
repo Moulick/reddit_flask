1c70a7c7c79efd3a2f95a4dd9141c57f20a5c1d9
Moulick Aggarwal <byesahil+github@gmail.com>
2017-04-08 05:19:12 +0530

    Added Materialize CSS

--- routes.py
+++ routes.py
@@ -4,21 +4,6 @@ import main_backend
 from reddit_flask import app
 
 
-# @app.route('/')
-# def landing():
-#     return 'ok'
-
-
-# @app.route('/<redditor>')
-# def count(redditor):
-#     common_word, number, total_comments = redditstuff.word(redditor)
-#
-#     return render_template('basic.html',
-#                            common_word=common_word,
-#                            number=number,
-#                            total_comments=total_comments,
-#                            redditor=redditor)
-
 
 @app.route('/', methods=['GET', 'POST'])
 def calculate():
