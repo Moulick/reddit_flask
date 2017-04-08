6eded264b4a53faf84ba1bf0488723f74e5c6507
Moulick Aggarwal <byesahil+github@gmail.com>
2017-04-09 00:22:52 +0530

    Removed material for testing purposes

--- reddit_flask.py
+++ reddit_flask.py
@@ -8,11 +8,4 @@ wsgi_app = app.wsgi_app
 from routes import *
 
 if __name__ == '__main__':
-    import os
-
-    HOST = os.environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT)
+    app.run(host='0.0.0.0', port='5000')
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+from reddit_flask import app
+
+if __name__ == "__main__":
+    app.run()
